c2a9ce0ed0414f4c823cb454a2b79d18f5184ae8
leo2735637 <82298612+leo2735637@users.noreply.github.com>
2021-05-02 11:44:27 +0800

    認識事件-欄位與方法

--- 2021_4_11_2D_Rockman/Assets/Scripts/Car.cs
+++ 2021_4_11_2D_Rockman/Assets/Scripts/Car.cs
@@ -2,6 +2,14 @@
 
 public class Car : MonoBehaviour
 {
+   #region 欄位
+     // 事件：在特定時間點會被執行的方法
+     // Unity 提供的事件：開始、更新
+
+     // 開始事件執行時間點與次數：播放遊戲後執行一次
+     // 應用：數值初始化，例如：遊戲一開始多少金幣或生命值等等...
+
+    
     //  雙斜線是單行註解    2021.4.25.leo
 
     /*  單斜線跟星星是多行註解
@@ -62,22 +70,100 @@ public class Car : MonoBehaviour
     public Vector3 v3 = new Vector3(1, 2, 3);
     public Vector4 v4 = new Vector4(1, 2, 3, 4);
 
-    //按鍵 KeyCode
-    public KeyCode key1;                                //  不指定為None(無)
-    public KeyCode key2 = KeyCode.A;
-    public KeyCode key3 = KeyCode.Mouse0;               //左 0 , 右 1 , 滾輪 2
-    public KeyCode key4 = KeyCode.Joystick1Button0;
+        //按鍵 KeyCode
+        public KeyCode key1;                                //  不指定為None(無)
+        public KeyCode key2 = KeyCode.A;
+        public KeyCode key3 = KeyCode.Mouse0;               //左 0 , 右 1 , 滾輪 2
+        public KeyCode key4 = KeyCode.Joystick1Button0;
 
-    //遊戲物件 與 元件
-    //遊戲物件 GameOdject
-    public GameObject obj1;
-    public GameObject obj2;
+        //遊戲物件 與 元件
+        //遊戲物件 GameOdject
+        public GameObject obj1;
+        public GameObject obj2;
 
-    //元件 Component - 屬性面板上可以摺疊的
-    //名稱去掉空格
-    public Transform tra;                     //可儲存任何包含 Transform 元件的物件
-    public SpriteRenderer spr;                //可儲存任何包含 SpriteRenderer 元件的物件
+        //元件 Component - 屬性面板上可以摺疊的
+        //名稱去掉空格
+        public Transform tra;                     //可儲存任何包含 Transform 元件的物件
+        public SpriteRenderer spr;                //可儲存任何包含 SpriteRenderer 元件的物件
 
+    #endregion
 
+   #region 事件 
+    public int unmber = 1;
+    public bool test = false;
+    public string prop = "紅色藥水";
+
+   // 事件：在特定時間點會被執行的方法
+   // Unity 提供的事件：開始、更新
+
+   // 開始事件執行時間點與次數：播放遊戲後執行一次
+   // 應用：數值初始化，例如：遊戲一開始多少金幣或生命值等等...
+   private void Start()
+   {
+       // print(任何資料) - 輸出資料到 Console 儀表板上
+       print("我是開始事件呦");
+
+        //  欄位存取
+        //  取得
+        //  語法：欄位名稱
+        //  字串串接：字串 + 其他欄位
+        print("取的欄位：" + unmber);
+
+        //  存放
+        //  語法：欄位名稱  指定  值 ;
+        //  值必須與此欄位類型相同
+        test = true;
+        print("存放欄位後的結果：" + test);
+
+        prop = "藍色藥水";
+        print("存放後的道具名稱：" + prop);
+
+        // 呼叫方法
+        // 方法名稱();
+        Test();
+
+        //傳回方法：
+        //傳回類型 名稱 = 傳回方法();
+        int t = Ten();
+        print("傳回方法的結果：" + t);
+
+   }
+
+   //更新事件執行時間點與次數：開始事件後以每秒約六十次執行 60FPS
+   //應用：監聽玩家輸入與物件持續行為，例如：玩家有沒有按按鈕或讓物件持續移動
+   private void Update()
+   {
+       print("我是更新事件!!!");   
+   }
+
+    #endregion
+
+   #region 方法  Unity Method、Function
+   // 方法：保存較複雜或演算法的程式區塊
+   // 語法：
+   // 修飾詞  傳回類型 名稱() { 較複雜演算法的程式區塊 }
+   // void  無傳回：使用這個方法不會傳回
+   //方法需要被【呼叫】才會被執行
+   /// <summary>
+   /// 我是一個測試方法
+   /// </summary>
+   
+   private void Test()
+    {
+        print("我是測試方法。");
+    }
+        
+    //如果不是無傳回 void
+    //在大括號內必須使用 傳回 return 值 (必須跟傳回類型相同)
+    /// <summary>
+    /// 傳回整數十的方法
+    /// </summary>
+    /// <returns>整數十</returns>
+    private int Ten()
+    {
+        return 10;
+    }
+
+   #endregion
 
 }
