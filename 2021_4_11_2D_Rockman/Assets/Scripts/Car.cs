12e5e2ca1c9f420ff2a771b51ea72165093cce6e
leo2735637 <82298612+leo2735637@users.noreply.github.com>
2021-05-02 13:00:04 +0800

    方法參數-預設值

--- 2021_4_11_2D_Rockman/Assets/Scripts/Car.cs
+++ 2021_4_11_2D_Rockman/Assets/Scripts/Car.cs
@@ -2,14 +2,14 @@
 
 public class Car : MonoBehaviour
 {
-   #region 欄位
-     // 事件：在特定時間點會被執行的方法
-     // Unity 提供的事件：開始、更新
+    #region 欄位
+    // 事件：在特定時間點會被執行的方法
+    // Unity 提供的事件：開始、更新
 
-     // 開始事件執行時間點與次數：播放遊戲後執行一次
-     // 應用：數值初始化，例如：遊戲一開始多少金幣或生命值等等...
+    // 開始事件執行時間點與次數：播放遊戲後執行一次
+    // 應用：數值初始化，例如：遊戲一開始多少金幣或生命值等等...
 
-    
+
     //  雙斜線是單行註解    2021.4.25.leo
 
     /*  單斜線跟星星是多行註解
@@ -42,14 +42,14 @@ public class Car : MonoBehaviour
     //  範圍  Range(最小值，最大值) - 限定數值類型
     [Header("汽車的 CC 數")]
     [Tooltip("這是汽車 cc 數")]
-    [Range(1000,5000)]
+    [Range(1000, 5000)]
     public int cc = 2000;
-    [Header("汽車的重量"), Tooltip("這是汽車重量。"),Range(0.5f, 10)]
-    public float   weight = 1.5f;
+    [Header("汽車的重量"), Tooltip("這是汽車重量。"), Range(0.5f, 10)]
+    public float weight = 1.5f;
     [Header("汽車的品牌")]
-    public string  brand = "BMW";
+    public string brand = "BMW";
     [Header("有沒有天窗")]
-    public bool    hasWindow = true;
+    public bool hasWindow = true;
 
     //  Unity  常見類型
     //  顏色  Color
@@ -66,42 +66,42 @@ public class Car : MonoBehaviour
     public Vector2 v2zero = Vector2.zero;
     public Vector2 v2one = Vector2.one;
     public Vector2 v2my = new Vector2(7, 9);
-    
+
     public Vector3 v3 = new Vector3(1, 2, 3);
     public Vector4 v4 = new Vector4(1, 2, 3, 4);
 
-        //按鍵 KeyCode
-        public KeyCode key1;                                //  不指定為None(無)
-        public KeyCode key2 = KeyCode.A;
-        public KeyCode key3 = KeyCode.Mouse0;               //左 0 , 右 1 , 滾輪 2
-        public KeyCode key4 = KeyCode.Joystick1Button0;
+    //按鍵 KeyCode
+    public KeyCode key1;                                //  不指定為None(無)
+    public KeyCode key2 = KeyCode.A;
+    public KeyCode key3 = KeyCode.Mouse0;               //左 0 , 右 1 , 滾輪 2
+    public KeyCode key4 = KeyCode.Joystick1Button0;
 
-        //遊戲物件 與 元件
-        //遊戲物件 GameOdject
-        public GameObject obj1;
-        public GameObject obj2;
+    //遊戲物件 與 元件
+    //遊戲物件 GameOdject
+    public GameObject obj1;
+    public GameObject obj2;
 
-        //元件 Component - 屬性面板上可以摺疊的
-        //名稱去掉空格
-        public Transform tra;                     //可儲存任何包含 Transform 元件的物件
-        public SpriteRenderer spr;                //可儲存任何包含 SpriteRenderer 元件的物件
+    //元件 Component - 屬性面板上可以摺疊的
+    //名稱去掉空格
+    public Transform tra;                     //可儲存任何包含 Transform 元件的物件
+    public SpriteRenderer spr;                //可儲存任何包含 SpriteRenderer 元件的物件
 
     #endregion
 
-   #region 事件 
+    #region 事件 
     public int unmber = 1;
     public bool test = false;
     public string prop = "紅色藥水";
 
-   // 事件：在特定時間點會被執行的方法
-   // Unity 提供的事件：開始、更新
+    // 事件：在特定時間點會被執行的方法
+    // Unity 提供的事件：開始、更新
 
-   // 開始事件執行時間點與次數：播放遊戲後執行一次
-   // 應用：數值初始化，例如：遊戲一開始多少金幣或生命值等等...
-   private void Start()
-   {
-       // print(任何資料) - 輸出資料到 Console 儀表板上
-       print("我是開始事件呦");
+    // 開始事件執行時間點與次數：播放遊戲後執行一次
+    // 應用：數值初始化，例如：遊戲一開始多少金幣或生命值等等...
+    private void Start()
+    {
+        // print(任何資料) - 輸出資料到 Console 儀表板上
+        print("我是開始事件呦");
 
         //  欄位存取
         //  取得
@@ -127,32 +127,43 @@ public class Car : MonoBehaviour
         int t = Ten();
         print("傳回方法的結果：" + t);
 
-   }
+        Drive50();
+        Drive100();
+        Drive300();
+        // 呼叫方法要有相同數量的參數
+        // 有預設值得參數為【填選式參數】
+        // 有預設值的參數只能寫在最右邊
+        Drive(200, "咻咻咻");
+        Drive(999, "轟轟轟", "爆炸特效");
+        //有多個選填式參數
+        Drive(70, "閃電");                //錯誤 - 把特效放在音效上  
+        Drive(70, effect: "閃電");        //正確 - 指定特效參數  
 
-   //更新事件執行時間點與次數：開始事件後以每秒約六十次執行 60FPS
-   //應用：監聽玩家輸入與物件持續行為，例如：玩家有沒有按按鈕或讓物件持續移動
-   private void Update()
-   {
-       print("我是更新事件!!!");   
-   }
+    }
 
+    //更新事件執行時間點與次數：開始事件後以每秒約六十次執行 60FPS
+    //應用：監聽玩家輸入與物件持續行為，例如：玩家有沒有按按鈕或讓物件持續移動
+    private void Update()
+    {
+        print("我是更新事件!!!");
+    }
     #endregion
 
-   #region 方法  Unity Method、Function
-   // 方法：保存較複雜或演算法的程式區塊
-   // 語法：
-   // 修飾詞  傳回類型 名稱() { 較複雜演算法的程式區塊 }
-   // void  無傳回：使用這個方法不會傳回
-   //方法需要被【呼叫】才會被執行
-   /// <summary>
-   /// 我是一個測試方法
-   /// </summary>
-   
-   private void Test()
+    #region 方法  Unity Method、Function
+    // 方法：保存較複雜或演算法的程式區塊
+    // 語法：
+    // 修飾詞  傳回類型 名稱(參數1，參數2，...參數n) { 較複雜演算法的程式區塊 }
+    // void  無傳回：使用這個方法不會傳回
+    //方法需要被【呼叫】才會被執行
+    /// <summary>
+    /// 我是一個測試方法
+    /// </summary>
+
+    private void Test()
     {
         print("我是測試方法。");
     }
-        
+
     //如果不是無傳回 void
     //在大括號內必須使用 傳回 return 值 (必須跟傳回類型相同)
     /// <summary>
@@ -164,6 +175,42 @@ public class Car : MonoBehaviour
         return 10;
     }
 
-   #endregion
+    // 舉例：
+    // 三個方法 1. 以時數 50 開車 2.時數 100 3. 時數 300
+    // 加新功能 要有音效
+    // 加特效
+    private void Drive50()
+    {
+        print("開車時速：" + 50);
+        print("開車音效");
+    }
+    private void Drive100()
+    {
+        print("開車時速：" + 100);
+        print("開車音效");
+    }
+    private void Drive300()
+    {
+        print("開車時速：" + 300);
+        print("開車音效");
+    }
+
+    // 用參數解決  Paramater
+    // 參數語法：類型 參數名稱
+    /// <summary>
+    /// 開車
+    /// </summary>
+    /// <param name="speed">開車的時速</param>
+    /// <param name="sound">開車的音效</param>
+    /// <param name="effect">特效</param>
+    //有預設值得參數只能寫在最右邊
+    private void Drive(int speed, string sound = "咻~", string effect = "灰塵效果")
+    {
+        print("開車時數：" + speed);
+        print("開車音效：" + sound);
+        print("特效：" + effect);
+    }
+
+    #endregion
 
 }
