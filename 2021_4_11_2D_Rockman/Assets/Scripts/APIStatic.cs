7a949fdcf12eab6878c91fc6728ab8dfc4d2e943
leo2735637 <82298612+leo2735637@users.noreply.github.com>
2021-05-02 17:04:03 +0800

    認是靜態與方法跟課堂練習

--- 2021_4_11_2D_Rockman/Assets/Scripts/APIStatic.cs
+++ 2021_4_11_2D_Rockman/Assets/Scripts/APIStatic.cs
@@ -7,6 +7,8 @@ public class APIStatic : MonoBehaviour
 
     private void Start()
     {
+        #region 認是靜態與方法
+
         // 屬性 欄位 要知道如何存取
         // 練習取得靜態屬性 Static  Properties
         // 語法
@@ -14,12 +16,45 @@ public class APIStatic : MonoBehaviour
         float r = Random.value;
         print("隨機值：" + r);
 
+
+        // 課堂練習取得靜態屬性 Static  Properties
+        float count = Camera.allCameras.Length;
+        print("we've got" + count + "cameras");
+
+
+
+
+
         // 練習存放靜態屬性 Static  Properties
         // 有顯示 Read Only 的屬性不能存放
         // 語法
         // 類別名稱.靜態屬性名稱 指定 值;
         Cursor.visible = false;            // 指標.可見度
 
+        // 練習使用靜態方法 Static Methods
+        // 語法
+        // 類別名稱.靜態方法(應對的參數)
+        int attack = Random.Range(100, 300);
+        print("隨機攻擊力：" + attack);
+
+        float n = Mathf.Abs(-99.9f);
+        print("絕對值結果：" + n);
+        #endregion
+
+
+        #region 練習
+        print("攝影機數量：" + Camera.allCameras);
+        print("重力：" + Physics2D.gravity);
+        #endregion
     }
 
+    private void Update()
+    {
+        #region 練習
+        print("玩家是否按下任意鍵：" + Input.anyKeyDown);
+        print("遊戲時間：" + Time.time);
+
+        #endregion
+
+    }
 }
