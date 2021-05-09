e8432a0a444239c01805c8c4d62499362130f49e
leo2735637 <82298612+leo2735637@users.noreply.github.com>
2021-05-09 11:52:43 +0800

    靜態程式跟開始-結束的程式

--- 2021_4_11_2D_Rockman/Assets/Scripts/APIStatic.cs
+++ 2021_4_11_2D_Rockman/Assets/Scripts/APIStatic.cs
@@ -5,6 +5,9 @@ public class APIStatic : MonoBehaviour
     //  認是靜態  API
     //  包含關鍵字  static 就是靜態
 
+    public Vector3 a = new Vector3(1, 1, 1);
+    public Vector3 b = new Vector3(22, 22, 22);
+
     private void Start()
     {
         #region 認是靜態與方法
@@ -40,20 +43,33 @@ public class APIStatic : MonoBehaviour
         float n = Mathf.Abs(-99.9f);
         print("絕對值結果：" + n);
         #endregion
-
-
+        
         #region 練習
         print("攝影機數量：" + Camera.allCameras);
         print("重力：" + Physics2D.gravity);
+
+        Physics2D.gravity = new Vector2(0, -20);
+        print("重力：" + Physics2D.gravity);
+
+        Application.OpenURL("https://unity.com/");
+        float f = Mathf.Floor(9.999f);
+        print("去掉小數點的結果：" + f);
+
+        float dis = Vector3.Distance(a, b);
+        print("A與B的距離：" + dis);
+
         #endregion
     }
 
     private void Update()
     {
         #region 練習
-        print("玩家是否按下任意鍵：" + Input.anyKeyDown);
-        print("遊戲時間：" + Time.time);
+        //print("玩家是否按下任意鍵：" + Input.anyKeyDown);
+        //print("遊戲時間：" + Time.time);
 
+        bool b = Input.GetKeyDown("space");
+        print("是否按下空白鍵：" + b);
+        
         #endregion
 
     }
